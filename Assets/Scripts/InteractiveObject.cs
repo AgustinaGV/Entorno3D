66431348cefe8842c89438da2ac4d720d23ebabd
Agustina <agustinagarciavega@gmail.com>
2021-09-02 15:54:20 -0300

    Update

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class InteractiveObject : MonoBehaviour
 {
     // Virtual = funciones que pueden ser extendidas, sobreescritas por otras clases
-   public virtual void OnSomethingEnter(GameObject go) {
-       
-   }
+    public int score;
+    public string objectName;
+    public Sprite thumb;
+    public int power;
 
-   public virtual void OnSomethingExit(GameObject go) {
-       
-   }
-
+    public virtual void OnInteract(Character character)
+    {
+        Pickup pickup = GetComponent<Pickup>();
+        if (pickup != null)
+        {
+            pickup.OnGrab(character);
+        }
+    }
+    public void UseIt()
+    {
+        Pickup pickup = GetComponent<Pickup>();
+        if (pickup != null)
+        {
+            pickup.Drop();
+        }
+    }
   
 
 }
